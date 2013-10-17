cb7eba543d354cf0938f2045537993abea213910
ph4r05 <ph4r05@gmail.com>
2013-10-17 15:31:02 +0200

    AES helper, aplha

---
+++ src/main/java/cz/muni/fi/xklinec/whiteboxAES/generator/AEShelper.java
@@ -0,0 +1,347 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.xklinec.whiteboxAES.generator;
+
+import cz.muni.fi.xklinec.whiteboxAES.State;
+import org.bouncycastle.pqc.math.linearalgebra.GF2Vector;
+import org.bouncycastle.pqc.math.linearalgebra.GF2mField;
+import org.bouncycastle.pqc.math.linearalgebra.GF2mMatrix;
+import org.bouncycastle.pqc.math.linearalgebra.Matrix;
+
+/**
+ *
+ * @author ph4r05
+ */
+public class AEShelper {
+    public static final int POLYNOMIAL     = 0x11B;
+    public static final int GENERATOR      = 0x03;
+    public static final int DEGREE         = 8;
+    public static final int AES_FIELD_SIZE = 1<<8;
+    
+    public static final byte testVect128_key[] = new byte[]{
+             (byte)0x2b, (byte)0x7e, (byte)0x15, (byte)0x16, (byte)0x28, (byte)0xae, 
+             (byte)0xd2, (byte)0xa6, (byte)0xab, (byte)0xf7, (byte)0x15, (byte)0x88, 
+             (byte)0x09, (byte)0xcf, (byte)0x4f, (byte)0x3c };
+
+    public static final byte testVect128_plain[][] = new byte[][]{
+            {(byte)0x32, (byte)0x43, (byte)0xf6, (byte)0xa8, (byte)0x88, (byte)0x5a, (byte)0x30, (byte)0x8d, 
+             (byte)0x31, (byte)0x31, (byte)0x98, (byte)0xa2, (byte)0xe0, (byte)0x37, (byte)0x07, (byte)0x34},
+            {(byte)0x6b, (byte)0xc1, (byte)0xbe, (byte)0xe2, (byte)0x2e, (byte)0x40, (byte)0x9f, (byte)0x96, 
+             (byte)0xe9, (byte)0x3d, (byte)0x7e, (byte)0x11, (byte)0x73, (byte)0x93, (byte)0x17, (byte)0x2a},
+            {(byte)0xae, (byte)0x2d, (byte)0x8a, (byte)0x57, (byte)0x1e, (byte)0x03, (byte)0xac, (byte)0x9c, 
+             (byte)0x9e, (byte)0xb7, (byte)0x6f, (byte)0xac, (byte)0x45, (byte)0xaf, (byte)0x8e, (byte)0x51},
+            {(byte)0x30, (byte)0xc8, (byte)0x1c, (byte)0x46, (byte)0xa3, (byte)0x5c, (byte)0xe4, (byte)0x11, 
+             (byte)0xe5, (byte)0xfb, (byte)0xc1, (byte)0x19, (byte)0x1a, (byte)0x0a, (byte)0x52, (byte)0xef},
+            {(byte)0xf6, (byte)0x9f, (byte)0x24, (byte)0x45, (byte)0xdf, (byte)0x4f, (byte)0x9b, (byte)0x17, 
+             (byte)0xad, (byte)0x2b, (byte)0x41, (byte)0x7b, (byte)0xe6, (byte)0x6c, (byte)0x37, (byte)0x10}
+    };
+
+    public static final byte testVect128_cipher[][] = new byte[][]{
+            {(byte)0x39, (byte)0x25, (byte)0x84, (byte)0x1d, (byte)0x02, (byte)0xdc, (byte)0x09, (byte)0xfb, 
+             (byte)0xdc, (byte)0x11, (byte)0x85, (byte)0x97, (byte)0x19, (byte)0x6a, (byte)0x0b, (byte)0x32},
+            {(byte)0x3a, (byte)0xd7, (byte)0x7b, (byte)0xb4, (byte)0x0d, (byte)0x7a, (byte)0x36, (byte)0x60, 
+             (byte)0xa8, (byte)0x9e, (byte)0xca, (byte)0xf3, (byte)0x24, (byte)0x66, (byte)0xef, (byte)0x97},
+            {(byte)0xf5, (byte)0xd3, (byte)0xd5, (byte)0x85, (byte)0x03, (byte)0xb9, (byte)0x69, (byte)0x9d, 
+             (byte)0xe7, (byte)0x85, (byte)0x89, (byte)0x5a, (byte)0x96, (byte)0xfd, (byte)0xba, (byte)0xaf},
+            {(byte)0x43, (byte)0xb1, (byte)0xcd, (byte)0x7f, (byte)0x59, (byte)0x8e, (byte)0xce, (byte)0x23, 
+             (byte)0x88, (byte)0x1b, (byte)0x00, (byte)0xe3, (byte)0xed, (byte)0x03, (byte)0x06, (byte)0x88},
+            {(byte)0x7b, (byte)0x0c, (byte)0x78, (byte)0x5e, (byte)0x27, (byte)0xe8, (byte)0xad, (byte)0x3f, 
+             (byte)0x82, (byte)0x23, (byte)0x20, (byte)0x71, (byte)0x04, (byte)0x72, (byte)0x5d, (byte)0xd4}
+    };
+
+    public static final byte testVect256_key[] = new byte[]{
+             (byte)0x60, (byte)0x3d, (byte)0xeb, (byte)0x10, (byte)0x15, (byte)0xca, (byte)0x71, (byte)0xbe, 
+             (byte)0x2b, (byte)0x73, (byte)0xae, (byte)0xf0, (byte)0x85, (byte)0x7d, (byte)0x77, (byte)0x81,
+             (byte)0x1f, (byte)0x35, (byte)0x2c, (byte)0x07, (byte)0x3b, (byte)0x61, (byte)0x08, (byte)0xd7, 
+             (byte)0x2d, (byte)0x98, (byte)0x10, (byte)0xa3, (byte)0x09, (byte)0x14, (byte)0xdf, (byte)0xf4
+    };
+
+    public static final byte testVect256_plain[][] = new byte[][]{
+            {(byte)0x6b, (byte)0xc1, (byte)0xbe, (byte)0xe2, (byte)0x2e, (byte)0x40, (byte)0x9f, (byte)0x96, 
+             (byte)0xe9, (byte)0x3d, (byte)0x7e, (byte)0x11, (byte)0x73, (byte)0x93, (byte)0x17, (byte)0x2a},
+            {(byte)0xae, (byte)0x2d, (byte)0x8a, (byte)0x57, (byte)0x1e, (byte)0x03, (byte)0xac, (byte)0x9c, 
+             (byte)0x9e, (byte)0xb7, (byte)0x6f, (byte)0xac, (byte)0x45, (byte)0xaf, (byte)0x8e, (byte)0x51},
+            {(byte)0x30, (byte)0xc8, (byte)0x1c, (byte)0x46, (byte)0xa3, (byte)0x5c, (byte)0xe4, (byte)0x11, 
+             (byte)0xe5, (byte)0xfb, (byte)0xc1, (byte)0x19, (byte)0x1a, (byte)0x0a, (byte)0x52, (byte)0xef},
+            {(byte)0xf6, (byte)0x9f, (byte)0x24, (byte)0x45, (byte)0xdf, (byte)0x4f, (byte)0x9b, (byte)0x17, 
+             (byte)0xad, (byte)0x2b, (byte)0x41, (byte)0x7b, (byte)0xe6, (byte)0x6c, (byte)0x37, (byte)0x10}
+    };
+
+    public static final byte testVect256_cipher[][] = new byte[][]{
+            {(byte)0xf3, (byte)0xee, (byte)0xd1, (byte)0xbd, (byte)0xb5, (byte)0xd2, (byte)0xa0, (byte)0x3c, 
+             (byte)0x06, (byte)0x4b, (byte)0x5a, (byte)0x7e, (byte)0x3d, (byte)0xb1, (byte)0x81, (byte)0xf8},
+            {(byte)0x59, (byte)0x1c, (byte)0xcb, (byte)0x10, (byte)0xd4, (byte)0x10, (byte)0xed, (byte)0x26, 
+             (byte)0xdc, (byte)0x5b, (byte)0xa7, (byte)0x4a, (byte)0x31, (byte)0x36, (byte)0x28, (byte)0x70},
+            {(byte)0xb6, (byte)0xed, (byte)0x21, (byte)0xb9, (byte)0x9c, (byte)0xa6, (byte)0xf4, (byte)0xf9, 
+             (byte)0xf1, (byte)0x53, (byte)0xe7, (byte)0xb1, (byte)0xbe, (byte)0xaf, (byte)0xed, (byte)0x1d},
+            {(byte)0x23, (byte)0x30, (byte)0x4b, (byte)0x7a, (byte)0x39, (byte)0xf9, (byte)0xf3, (byte)0xff, 
+             (byte)0x06, (byte)0x7d, (byte)0x8d, (byte)0x8f, (byte)0x9e, (byte)0x24, (byte)0xec, (byte)0xc7}
+    };
+    
+    protected int g[]             = new int[AES_FIELD_SIZE];
+    protected int gInv[]          = new int[AES_FIELD_SIZE];
+    protected int sbox[]          = new int[AES_FIELD_SIZE];
+    protected int sboxAffine[]    = new int[AES_FIELD_SIZE];
+    protected int sboxAffineInv[] = new int[AES_FIELD_SIZE];
+    
+    protected int mixColModulus[]      = new int[5];
+    protected int mixColMultiply[]     = new int[4];
+    protected int mixColMultiplyInv[]  = new int[4];
+    
+    protected GF2mMatrix mixColMat;
+    protected GF2mMatrix mixColInvMat;
+    
+    public void build(boolean encrypt){
+        GF2mField field = new GF2mField(8, POLYNOMIAL);
+        System.out.println(field);
+        
+        int i,cur = 1;
+	gInv[0] = -1;
+	for(i=0; i<AES_FIELD_SIZE; i++){
+            g[i] = cur;
+            gInv[cur] = i;
+            field.mult(cur, GENERATOR);
+	}
+        
+        // 2. compute GF(256) element inverses in terms of generator exponent
+	sbox[0] = -1;
+	for(i=1; i<AES_FIELD_SIZE; i++){
+            sbox[i] = 255-i;
+	}
+        
+        GF2MatrixEx tmpM = new GF2MatrixEx(8, 1);
+        GF2MatrixEx afM  = getDefaultAffineMatrix(encrypt);
+        byte        afC  = getDefaultAffineConstByte(encrypt);
+        
+        
+        // Computing whole Sboxes with inversion + affine transformation in generic AES
+        // Normal Sbox:      S(x) = const    +   A(x^{-1})
+	// Sbox in Dual AES: G(x) = T(const) + T(A(T^{-1}(x^{-1})))
+	for(i=0; i<AES_FIELD_SIZE; i++){
+            int tmpRes;
+
+            // i is now long representation, gInv transforms it to exponent power to obtain inverse.
+            // Also getLong(g[gInv[i]]) == i
+            int transValue = i==0 ? 0 : g[255-gInv[i]];
+
+            // tmpM = col vector of transValue
+            NTLUtils.zero(tmpM);
+            NTLUtils.putByteAsColVector(tmpM, (byte)transValue, 0, 0);
+            
+            // const + A(x^{-1})
+            GF2MatrixEx resMatrix = (GF2MatrixEx) afM.rightMultiply(tmpM);
+            tmpRes = field.add(NTLUtils.ColBinaryVectorToByte(resMatrix, 0, 0), afC);
+            
+            //transValue = 
+            sboxAffine[i] = tmpRes;
+            sboxAffineInv[tmpRes] = i;
+
+            // Inversion, idea is the same, i is the long representation of element in GF, apply inverted affine transformation and take inverse
+            // Ax^{-1} + c is input to this transformation
+            //              [A^{-1} * (A{x^-1} + c) + d]^{-1} is this transformation;
+            // correctness: [A^{-1} * (Ax^-1   + c) + d]^{-1} =
+            //				[A^{-1}Ax^{-1} + A^{-1}c + d]^{-1} =	//	A^{-1}c = d
+            //				[x^{-1}        + 0]^{-1} =
+            //				x
+            //
+            // Computation is useless, we have inversion of transformation right from transformation above
+            // by simply swapping indexes. This is just for validation purposes to show, that it really works and how
+	}
+
+	// 6. MixColumn operations
+	// modulus x^4 + 1
+	mixColModulus[0] = g[0];
+	mixColModulus[4] = g[0];
+
+	// 03 x^3 + 01 x^2 + 01 x + 02
+	mixColMultiply[0] = g[25];
+	mixColMultiply[1] = g[0];
+	mixColMultiply[2] = g[0];
+	mixColMultiply[3] = g[1];
+
+	// inverse polynomial
+	mixColMultiplyInv[0] = g[223];
+	mixColMultiplyInv[1] = g[199];
+	mixColMultiplyInv[2] = g[238];
+	mixColMultiplyInv[3] = g[104];
+
+	// MixCols multiplication matrix based on mult polynomial -  see Rijndael description of this.
+	// Polynomials have coefficients in GF(256).
+	mixColMat = new GF2mMatrixSetDims(4,4);
+	mixColInvMat.SetDims(4,4);
+	for(i=0; i<4; i++){
+		for(c=0; c<4; c++){
+			mixColMat.put(i, c, mixColMultiply[(i+4-c) % 4]);
+			mixColInvMat.put(i, c, mixColMultiplyInv[(i+4-c) % 4]);
+		}
+	}
+
+	// Round key constant RC (for key schedule) obeys this reccurence:
+	// RC[0] = 1
+	// RC[i] = '02' * RC[i-1] = x * RC[i-1] = x^{i-1} `mod` R(X)
+	RC[0] = g[0];
+	for(i=1; i<16; i++){
+		RC[i] =  g[25] * RC[i-1];
+	}
+    }
+    
+    /**
+     * Number of rounds of AES depends on key size
+     */
+    public static int getNumberOfRounds(int keySize){ 
+        return keySize/4+6; 
+    }
+    
+    /**
+     * Positive modulo 4
+     * @param a
+     * @return 
+     */
+    public static int mod4(int a){ 
+        int c = a % 4; 
+        return c<0 ? c+4 : c; 
+    }
+    
+    /**
+     * Returns default affine matrix transformation for S-box.
+     * @param encrypt
+     * @return 
+     */
+    public static GF2MatrixEx getDefaultAffineMatrix(boolean encrypt){
+        GF2MatrixEx r = new GF2MatrixEx(8, 8);
+        
+        if (encrypt){
+            NTLUtils.putByteAsRowVector(r, (byte)0x8F, 0, 0);
+            NTLUtils.putByteAsRowVector(r, (byte)0xC7, 1, 0);
+            NTLUtils.putByteAsRowVector(r, (byte)0xE3, 2, 0);
+            NTLUtils.putByteAsRowVector(r, (byte)0xF1, 3, 0);
+            NTLUtils.putByteAsRowVector(r, (byte)0xF8, 4, 0);
+            NTLUtils.putByteAsRowVector(r, (byte)0x7C, 5, 0);
+            NTLUtils.putByteAsRowVector(r, (byte)0x3E, 6, 0);
+            NTLUtils.putByteAsRowVector(r, (byte)0x1F, 7, 0);
+        } else {
+            NTLUtils.putByteAsRowVector(r, (byte)0x25, 0, 0);
+            NTLUtils.putByteAsRowVector(r, (byte)0x92, 1, 0);
+            NTLUtils.putByteAsRowVector(r, (byte)0x49, 2, 0);
+            NTLUtils.putByteAsRowVector(r, (byte)0xA4, 3, 0);
+            NTLUtils.putByteAsRowVector(r, (byte)0x52, 4, 0);
+            NTLUtils.putByteAsRowVector(r, (byte)0x29, 5, 0);
+            NTLUtils.putByteAsRowVector(r, (byte)0x94, 6, 0);
+            NTLUtils.putByteAsRowVector(r, (byte)0x4A, 7, 0);         
+        }
+        
+        return r;
+    }
+    
+    /**
+     * Default affine constant for affine transformation for S-box.
+     * @param encrypt
+     * @return 
+     */
+    public static byte getDefaultAffineConstByte(boolean encrypt){
+        return encrypt ? (byte)0x63 : (byte)0x05;
+    }
+    
+    /**
+     * Returns affine constant for affine transformation for S-box as a col vector.
+     * @param encrypt
+     * @return 
+     */
+    public static GF2MatrixEx getDefaultAffineConst(boolean encrypt){
+        GF2MatrixEx r = new GF2MatrixEx(8,1);
+        NTLUtils.putByteAsColVector(r, getDefaultAffineConstByte(encrypt), 0, 0);
+        return r;
+    }
+    
+    /**
+     * AES key schedule.
+     * 
+     * @param expandedKey
+     * @param key
+     * @param keySize 
+     */
+    public static void expandKey(GF2Vector expandedKey, byte[] key, int size, boolean debug){
+        /* current expanded keySize, in bytes */
+	int currentSize = 0;
+	int rconIteration = 0;
+	int i,j;
+	int Nr = getNumberOfRounds(size);
+	int expandedKeySize = (4 * State.COLS * (Nr + 1));
+//
+//	GF2E tmp;
+//	vec_GF2E t(INIT_SIZE, 4);
+//
+//	 result expanded key size = NB * (NK + 1)
+//	if (debug){
+//            System.out.println("Expanded key size will be: " + expandedKeySize);
+//        }
+//        
+//	expandedKey.SetLength(expandedKeySize);
+//
+//	/* set the 16,24,32 bytes of the expanded key to the input key */
+//	for (i = 0; i < size; i++)
+//		expandedKey[i] = key[i];
+//
+//	currentSize += size;
+//	while (currentSize < expandedKeySize){
+//		if (debug){
+//                    System.out.println("CurrentSize: " + currentSize + "; expandedKeySize: " + expandedKeySize);
+//                }
+//                
+//		/* assign the previous 4 bytes to the temporary value t */
+//		for (i = 0; i < 4; i++) {
+//			t[i] = expandedKey[(currentSize - 4) + i];
+//		}
+//
+//		/**
+//		 * every 16,24,32 bytes we apply the core schedule to t
+//		 * and increment rconIteration afterwards
+//		 */
+//		if(currentSize % size == 0) {
+//			core(t, rconIteration++);
+//			/* rotate the 32-bit word 8 bits to the left */
+//			tmp=t[0]; 	t[0]=t[1];
+//			t[1]=t[2];	t[2]=t[3];
+//			t[3]=tmp;
+//			/* apply S-Box substitution on all 4 parts of the 32-bit word */
+//			for (j = 0; j < 4; ++j){
+//				if (debug){
+//                                    System.out.println("Sboxing key t[" + j + "]=" + t[j] + "=" << GF2EHEX(t[j]) + "; sboxval: " + CHEX(sboxAffine[getLong(t[j])])) ;
+//                                }
+//				
+//                                t[j] = GF2EFromLong(sboxAffine[getLong(t[j])], AES_FIELD_DIM);
+//				
+//                                if (debug){
+//                                    System.out.println(" after Sbox = " + t[j] + "="  << GF2EHEX(t[j]));
+//                                }
+//			}
+//			/* XOR the output of the rcon operation with i to the first part (leftmost) only */
+//			t[0] = t[0] + RC[rconIteration++];
+//
+//			if (debug){
+//			    System.out.println("; after XOR with RC[" + GF2EHEX(RC[rconIteration-1]) + "] = " + t[0] + " = " + GF2EHEX(t[0]));
+//                        }
+//		}
+//
+//		/* For 256-bit keys, we add an extra sbox to the calculation */
+//		if(size == KEY_SIZE_32 && ((currentSize % size) == 16)) {
+//			for(i = 0; i < 4; i++)
+//				t[i] = GF2EFromLong(sboxAffine[getLong(t[i])], AES_FIELD_DIM);
+//		}
+//		/* We XOR t with the four-byte block 16,24,32 bytes before the new expanded key.
+//		* This becomes the next four bytes in the expanded key.
+//		*/
+//		for(i = 0; i < 4; i++) {
+//			expandedKey[currentSize] = expandedKey[currentSize - size] + t[i];
+//
+//			#ifdef GENERIC_AES_DEBUG
+//			cout << "t[" << i << "] = " << GF2EHEX(t[i]) << endl;
+//			#endif
+//
+//			currentSize++;
+//		}
+//	}
+    }
+}
